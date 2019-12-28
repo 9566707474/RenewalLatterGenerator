88dea936619b54ef82302b6060b798f267cea130
Rajendran <585696@cognizant.com>
2019-12-28 13:46:30 +0530

    InitialMonthlyPaymentAmountRuleTest added

--- RenewalLatterGenerator/Features/Rules/InitialMonthlyPaymentAmountRule.cs
+++ RenewalLatterGenerator/Features/Rules/InitialMonthlyPaymentAmountRule.cs
@@ -1,5 +1,6 @@
 ﻿namespace RenewalLatterGenerator.Features.Rules
 {
+    using RenewalLatterGenerator.Exceptions;
     using RenewalLatterGenerator.Models;
     using System;
 
@@ -7,6 +8,10 @@
     {
         public CustomerProduct Apply(CustomerProduct customerProduct)
         {
+            if (customerProduct?.AverageMonthlyPremium == null)
+            {
+                throw new RuleException("Customer product average monthly premium not found");
+            }
             var amount = customerProduct.AverageMonthlyPremium - Math.Round(customerProduct.AverageMonthlyPremium, 2);
 
             customerProduct.InitialMonthlyPayment = Math.Round((amount * 12) + customerProduct.AverageMonthlyPremium, 2);
