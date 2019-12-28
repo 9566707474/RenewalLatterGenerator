57e7d6a5640b60288ec148cd1087284f9fccb482
Rajendran <585696@cognizant.com>
2019-12-28 17:19:35 +0530

    DataExtractor Test

--- RenewalLatterGenerator/Features/DataExtractor/DataExtractor.cs
+++ RenewalLatterGenerator/Features/DataExtractor/DataExtractor.cs
@@ -1,22 +1,27 @@
 ﻿namespace RenewalLatterGenerator.Features.DataExtractor
 {
-    using RenewalLatterGenerator.Common;
     using RenewalLatterGenerator.Infrastructure;
     using RenewalLatterGenerator.Models;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Used to extract the customer product data from the given file
+    /// </summary>
     public class DataExtractor : IDataExtractor
     {
         private readonly IFileHandlerResolver fileHandlerResolver;
 
-        private readonly IFileSystem fileSystem;
-
-        public DataExtractor(IFileHandlerResolver fileHandlerResolver, IFileSystem fileSystem)
+        public DataExtractor(IFileHandlerResolver fileHandlerResolver)
         {
             this.fileHandlerResolver = fileHandlerResolver;
-            this.fileSystem = fileSystem;
         }
 
+        /// <summary>
+        /// Get customer products from the given file
+        /// </summary>
+        /// <param name="fileHandlerType">File handler type</param>
+        /// <param name="filePath">File path</param>
+        /// <returns>Collection of CustomerProduct</returns>
         public ICollection<CustomerProduct> GetCustomerProductsFromFile(string fileHandlerType, string filePath)
         {
             var fileHandler = this.fileHandlerResolver.Resolve(fileHandlerType);
