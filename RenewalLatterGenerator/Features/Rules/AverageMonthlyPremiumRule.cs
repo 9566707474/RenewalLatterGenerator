17b1cf1e7ea0437d63df5c636347c58da37d02f6
Rajendran <585696@cognizant.com>
2019-12-28 13:17:39 +0530

    AverageMonthlyPremiumRule Test class added

--- RenewalLatterGenerator/Features/Rules/AverageMonthlyPremiumRule.cs
+++ RenewalLatterGenerator/Features/Rules/AverageMonthlyPremiumRule.cs
@@ -1,11 +1,16 @@
 ﻿namespace RenewalLatterGenerator.Features.Rules
 {
+    using RenewalLatterGenerator.Exceptions;
     using RenewalLatterGenerator.Models;
 
     public class AverageMonthlyPremiumRule : IRule
     {
         public CustomerProduct Apply(CustomerProduct customerProduct)
         {
+            if (customerProduct?.TotalPremium == null)
+            {
+                throw new RuleException("Customer product total premium not found");
+            }
             customerProduct.AverageMonthlyPremium = customerProduct.TotalPremium / 12;
             return customerProduct;
         }
